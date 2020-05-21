2012eb6c5712f470f54528579dfd2b8a9da671b0
charl25 <sirayisinekhaya@gmail.com>
2020-05-21 22:11:39 +0200

    another test

---
+++ test.isFromBellville.js
@@ -0,0 +1,10 @@
+describe("The isFromBellville function ", function() {
+    
+    it("should return true for CY 123-223", function() {
+      assert.equal(true, isFromBellville("CY 123-223"));
+    });
+    
+    it("should return false for CJ 123-223", function() {
+      assert.equal(false, isFromBellville("CJ 123-223"));
+    });
+  });
